a91a7f9e8279ef3d5dc30c4025398de22d35dee5
Geral <Geral@DESKTOP-BADHE74>
2023-11-10 09:56:45 -0300

    Melhoria no relatório Correção no vencimento Implementação produtos registrados estoque

--- src/model/vencimento.java
+++ src/model/vencimento.java
@@ -7,6 +7,9 @@ public class vencimento {
     public String data;
     public String telefone;
     public String vencimento;
+    public String clienteold;
+    public String dataold;
+    public String vencimentoold;
 
     public String getCliente() {
         return cliente;
@@ -46,6 +49,30 @@ public class vencimento {
 
     public void setVencimento(String vencimento) {
         this.vencimento = vencimento;
+    }
+
+    public String getClienteold() {
+        return clienteold;
+    }
+
+    public void setClienteold(String clienteold) {
+        this.clienteold = clienteold;
+    }
+
+    public String getDataold() {
+        return dataold;
+    }
+
+    public void setDataold(String dataold) {
+        this.dataold = dataold;
+    }
+
+    public String getVencimentoold() {
+        return vencimentoold;
+    }
+
+    public void setVencimentoold(String vencimentoold) {
+        this.vencimentoold = vencimentoold;
     }
 
 }
